85baa254e8c0135b564baef3da0739e452d5e51d
The Real Žvýd <mystrdat@gmail.com>
2023-01-05 23:00:00 +0100

    initial

---
+++ script.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name        No Verbal on ČSFD.cz
+// @namespace   Violentmonkey Scripts
+// @match       https://*.csfd.cz/*
+// @run-at      document-idle
+// @version     1.0
+// @author      mystrdat
+// @description Removes comments from verbal (or anyone else) on CSFD.cz
+// @homepageURL https://github.com/mystrdat/csfd-no-verbal
+// @icon        https://www.google.com/s2/favicons?domain=csfd.cz
+// ==/UserScript==
+
+(function() {
+  const users = ['verbal'];
+  const comments = document.querySelectorAll('#snippet--comments > article');
+  const ratings = document.querySelectorAll('.aside-movie-profile .user-list li');
+  // Delete comments
+  comments.forEach((comment) => {
+    users.includes(comment.querySelector('.user-title-name').childNodes[0].nodeValue) && comment.remove();
+  });
+  // Delete ratings
+  ratings.forEach((rating) => {
+    users.includes(rating.querySelector('a').childNodes[0].nodeValue) && rating.remove();
+  });
+})();
